4c3266329eb2d3346f2a6d63c7f8b16e8a4a64e8
itzciujis <itzciujis@gmail.com>
2025-11-26 12:07:11 +0100

    modifiacion

--- AgendaContacto/src/Main.java
+++ AgendaContacto/src/Main.java
@@ -1,19 +1,41 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void  main(String[] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int opcion = 0;
 
-        System.out.println("Añadir contacto");
-        System.out.println("Mostrar contacto");
-        System.out.println("Buscar contacto");
-        System.out.println("Salir");
+        while (opcion != 4) {
 
-        System.out.println("Elija una opcion");
+            System.out.println("------ AGENDA DE CONTACTOS ------");
+            System.out.println("1. Añadir contacto");
+            System.out.println("2. Mostrar contacto");
+            System.out.println("3. Buscar contacto");
+            System.out.println("4. Salir");
+            System.out.print("Elija una opción: ");
 
-        String opciones = sc.next();
-        String opciones1 = opciones;
-        System.out.println("Has elejido la opcion: " + opciones1);
+            opcion = sc.nextInt();
 
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has elegido: Añadir contacto");
+                    break;
+                case 2:
+                    System.out.println("Has elegido: Mostrar contacto");
+                    break;
+                case 3:
+                    System.out.println("Has elegido: Buscar contacto");
+                    break;
+                case 4:
+                    System.out.println("Saliendo de la agenda...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+
+            System.out.println();
+        }
+        
     }
 }
